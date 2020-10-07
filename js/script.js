aa02c9d1f6581f7d0d5ba3fad82e00953f3f9a2f
Natalia Smola <smolutka15@gmail.com>
2020-10-07 17:29:46 +0200

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 });*/
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
   /* remove class 'active from all article links'*/
@@ -13,13 +14,15 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* remove class 'active' from all articles */
-
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
