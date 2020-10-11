39d07f1159d44463e497b064eef9f542b18b3240
Natalia Smola <smolutka15@gmail.com>
2020-10-11 20:23:27 +0200

    Fix the bug in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict'
 const titleClickHandler = function(event) {
   event.preventDefault();
   const clickedElement = this;
@@ -30,8 +31,43 @@ const titleClickHandler = function(event) {
   console.log('targetArticle', targetArticle);
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
+
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleLinksSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove content of titleList */
+const titleList = document.querySelector(optTitleLinksSelector);
+
+titleList.innerHTML = '';
+
+  /*for each article*/
+const articles = document.querySelectorAll(optArticleSelector);
+
+let html ='';
+
+for(let article of articles){
+    /*get the article id*/
+    const articleId = article.getAttribute('id');
+    /*find the title element*/
+    /*get the title from the title element*/
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /*create HTML of the link*/
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /*insert link into titleList*/
+  //const titleListLink = article.insertAdjacentHTML('afterend', linkHTML);
+  //console.log(titleListLink);
+    html = html + linkHTML
+  }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+generateTitleLinks();
